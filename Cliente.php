bafba4a5e9663db23d8782c92d3fd988dfbb894f
lautaromorales <lautamorales123@gmail.com>
2024-04-10 22:49:23 -0300

    Subida

---
+++ Cliente.php
@@ -0,0 +1,66 @@
+<?php
+//CLASE CLIENTE
+
+class CLiente{
+    private $nombre;
+    private $apellido;
+    private $dadoDeBaja;
+    private $tipoDoc;
+    private $nroDoc;
+    
+    //** MÉTODO MÁGICO CONSTRUCT  **//
+    public function __construct($name, $pApellido, $pBaja, $tipoId, $nroId){
+        $this->nombre = $name;
+        $this->apellido = $pApellido;
+        $this->dadoDeBaja = $pBaja;
+        $this->tipoDoc = $tipoId;
+        $this->nroDoc = $nroId;        
+    }
+    //** MÉTODOS MÁGICOS "GETTERS" **//
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function getApellido(){
+        return $this->apellido;
+    }
+    public function getBaja(){
+        return $this->dadoDeBaja;
+    }
+    public function getTipoDoc(){
+        return $this->tipoDoc;
+    }
+    public function getNumDoc(){
+        return $this->nroDoc;
+    }
+
+    //** MÉTODOS MÁGICOS "GETTERS" **//
+    public function setNombre($p_Nombre){
+        $this->nombre = $p_Nombre;
+    }
+    public function setApellido ($p_Apellido){
+        $this->apellido = $p_Apellido;
+    }
+    public function setBaja ($p_Baja){
+        $this->dadoDeBaja = $p_Baja;
+    }
+    public function setTipoDoc($p_Tipo_Doc){
+        $this->tipoDoc = $p_Tipo_Doc;
+    }
+    public function setNumDoc ($p_Num_Doc){
+        $this->nroDoc = $p_Num_Doc;
+    }
+
+    //** MÉTODO MÁGICO __toString **//
+    public function __toString(){
+        if ($this->getBaja()){
+            $estadoCliente = "Alta activa";
+        }else{
+            $estadoCliente = "Dado de baja";
+        }
+        return "Nombre: ".$this->getNombre()."\n Apellido: ".$this->getApellido()."\n Estado ".$estadoCliente.
+        "\n Número de documento: ".$this->getNumDoc()."\n Tipo de documento ".$this->getTipoDoc();       
+    }
+
+}
+
+echo $this->getNombre();
---
+++ Moto.php
@@ -0,0 +1,66 @@
+<?php
+//CLASE MOTO
+class Moto{
+    private $codigo;
+    private $costo;
+    private $descripcion;
+    private $por_inc_anual;
+    private $activa;
+    private $anioFabricacion;
+
+    public function __construct($codMoto, $costoMoto, $descMoto, $porcMoto, $activaMoto, $anioMoto){
+        $this->codigo = $codMoto;
+        $this->costo = $costoMoto;
+        $this->descripcion = $descMoto;
+        $this->por_inc_anual = $porcMoto;
+        $this->activa = $activaMoto;
+        $this->anioFabricacion = $anioMoto;
+    }
+
+
+    public function getCodigo(){
+        return $this->codigo ;
+    }
+    public function getCosto(){
+        return $this->costo;
+    }
+    public function getDescripcion(){
+        return $this->descripcion;
+    }
+    public function getPorcentaje(){
+        return $this->por_inc_anual;
+    }
+    public function getEstadoMoto(){
+        return $this->activa;
+    }
+    public function getAnioMoto(){
+        return $this->anioFabricacion;
+    }
+    //** MÉTODOS MÁGICOS "GETTERS" **//
+    public function setCodigo($p_codigo){
+        $this->codigo =$p_codigo;
+    }
+    public function setCosto($p_Costo){
+        $this->costo = $p_Costo;
+    }
+    public function setDesccripcion($p_Descripcion){
+        $this->descripcion =$p_Descripcion;
+    }
+    public function setPorcentaje($p_Porcentaje){
+        $this->por_inc_anual =$p_Porcentaje;
+    }
+    public function setEstadoMoto($p_Estado){
+        $this->activa=$p_Estado;
+    }
+    public function setAnio($p_Anio){
+        $this->anioFabricacion =$p_Anio;
+    }
+
+    //** MÉTODO MÁGICO "toString" **//
+    public function __toString()
+    {
+        return "Código: ".$this->getCodigo()."\n Costo: ".$this->getCosto()."\n Descripcion: ".$this->getDescripcion().
+        "\n Porcentaje de incremento anual: ".$this->getPorcentaje()."\n Estado para la venta: ".$this->getEstadoMoto().
+        "\n Año de Fabricación: ".$this->getAnioMoto();
+    }
+}
